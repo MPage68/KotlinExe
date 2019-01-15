cc161ed918cbaf6c416b27e7371f838fda5689e6
Michael Page <mikepage68@live.com>
2019-01-15 16:46:01 -0500

    fun

--- src/KotlinExe.kt
+++ src/KotlinExe.kt
@@ -2,9 +2,11 @@ package demo
 
 
 import kotlin.random.Random
+import kotlin.Pair
 
 
-fun main(args : Array<String>){
+
+fun main(args : Array<String>) {
     println("Hello, world!")
 
     //Number values
@@ -78,7 +80,7 @@ fun main(args : Array<String>){
     println("Strings Equal : ${str1.equals(str2)}")
     println("Compare A to B : ${'A'.compareTo('B')}")
     println("2nd index : ${str1[2]}")
-    println("Index 2 - 7 : ${str1.subSequence(2,8)}")
+    println("Index 2 - 7 : ${str1.subSequence(2, 8)}")
     println("Contains random : ${str1.contains("random")}")
 
     //Arrays
@@ -92,16 +94,16 @@ fun main(args : Array<String>){
     println("Array Length : ${myArray.size}")
     println("Mike in Array : ${myArray.contains("Mike")}")
 
-    var partArray = myArray.copyOfRange(0,1)
+    var partArray = myArray.copyOfRange(0, 1)
 
     println("First : ${myArray.first()}")
     println("Index of Mike : ${myArray.indexOf("Mike")}")
 
     //Square array { x = 2, then 2 x 2 }
-    var sqArray = Array(5, { x -> x * x})
+    var sqArray = Array(5, { x -> x * x })
     println(sqArray[2])
     //type specific Array <type>
-    var arr2: Array<Int> = arrayOf(1,2,3)
+    var arr2: Array<Int> = arrayOf(1, 2, 3)
 
     //Ranges
 
@@ -114,61 +116,116 @@ fun main(args : Array<String>){
     val twoTo20 = 2.rangeTo(20)
     val rng3 = oneTo10.step(3)
     //for loop
-    for(x in rng3) println("rng3 : $x")
-    for(x in tenTo1.reversed()) println("Reverse : $x")
+    for (x in rng3) println("rng3 : $x")
+    for (x in tenTo1.reversed()) println("Reverse : $x")
 
     //Conditionals
 
     val age = 8
 
-    if(age < 5){
+    if (age < 5) {
         println("Go to preschool")
-    }else if(age == 5){
+    } else if (age == 5) {
         println("Go to kindergarden")
-    }else if((age > 5) && (age <= 17)){
+    } else if ((age > 5) && (age <= 17)) {
         val grade = age - 5
         println("Go to Grade $grade")
-    }else{
+    } else {
         println("Go to College")
     }
     //When statement, similar to Case in Java
-    when(age){
-        0,1,2,3,4 -> println("Go to Preschool")
+    when (age) {
+        0, 1, 2, 3, 4 -> println("Go to Preschool")
 
         5 -> println("Go to Kindergarden")
         //in denotes range
         in 6..17 -> {
-            val grade = age -5
+            val grade = age - 5
             println("Go to grade $grade")
         }
         else -> println("Go to college")
     }
 
     //Loops
-    for(x in 1..10){
+    for (x in 1..10) {
         println("Loop : $x")
     }
     //import Kotlin Random number -> does not use "()" on instance of .Random
-    val rand : Random = Random
+    val rand: Random = Random
     val magicNum = rand.nextInt(50) + 1
 
     var guess = 0
 
-     while(magicNum != guess){
-         guess += 1
-     }
+    while (magicNum != guess) {
+        guess += 1
+    }
 
     println("Magic Number was $guess")
 
-        for(x in 1..20){
-            if(x % 2 == 0){
-                continue
-                //skips even numbers and prints odd
-            }
-            println("Odd : $x")
+    for (x in 1..20) {
+        if (x % 2 == 0) {
+            continue
+            //skips even numbers and prints odd
+        }
+        println("Odd : $x")
 
-            if(x == 15) break
+        if (x == 15) break
+    }
+    //for loop used to iterate through an array to display each index
+    var arr3: Array<Int> = arrayOf(3, 6, 9)
 
+    for (i in arr3.indices) {
+        println("Mult 3 : ${arr3[i]}")
+    }
+
+    for ((index, value) in arr3.withIndex()) {
+        println("Index : $ Value : $value")
+    }
+
+    //functions
+
+    fun add(num1: Int, num2: Int): Int = num1 + num2
+    println("5+4 = ${add(5, 4)}")
+
+    fun subtract(num1: Int = 1, num2: Int = 1) = num1 - num2
+    println("5 - 4 = ${subtract(5, 4)}")
+    println("4 - 5 = ${subtract(num2 = 5, num1 = 4)}")
+
+    fun sayHello(name: String): Unit = println("Hello $name")
+    sayHello("Mike")
+
+   // val (two, three) = nextTwo(1)
+    //println("1 $two $three")
+
+    println("Sum = ${getSum(1,2,3,4,5)}")
+
+    val multiply = {num1: Int, num2: Int -> num1 * num2}
+
+    println("5 * 3 = ${multiply(5,3)}")
+    //factoral "!" operator
+    println("5! = ${fact(5)}")
+
+
+
+}
+
+    /*fun nextTwo(num: Int): Pair<Int, Int>{
+        return Pair(num+1, num+2)
+    }*/
+    fun getSum(vararg nums: Int): Int{
+        var sum = 0
+
+        nums.forEach{n -> sum += n}
+
+        return sum
+    }
+    //factoral recursion
+    fun fact(x: Int): Int{
+        tailrec fun factTail(y: Int, z: Int): Int {
+            if(y == 0) return z
+            else return factTail(y - 1, y * z)
+        }
+        return factTail(x, 1)
     }
 
 
@@ -179,7 +236,5 @@ fun main(args : Array<String>){
 
 
 
-
-}
 
 
